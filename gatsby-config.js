727e6c4dce249ef60ae47bcc2773ecf1dd12e3fe
Ricky Chon <rickychon99@gmail.com>
2022-01-09 18:14:41 -0500

    Update favicon (#3)

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,19 +114,19 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Brain Kosmos`,
-        short_name: `GatsbyJS`,
+        short_name: `BK`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/profile-pic-cropped.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
