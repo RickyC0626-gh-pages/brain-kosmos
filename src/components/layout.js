8439df00654aa6912de81914b4bd4e53fa429493
Ricky Chon <rickychon99@gmail.com>
2022-01-09 22:11:22 -0500

    Add dark/light mode toggle (#5)

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,20 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import Toggle from "react-toggle"
+
+import sun from '../images/sun.png'
+import moon from '../images/moon.png'
 
 const Layout = ({ location, title, children }) => {
+  const [theme, setTheme] = React.useState(null);
+
+  React.useEffect(() => {
+    console.log('componentDidMount')
+    setTheme(window.__theme);
+  
+    window.__onThemeChange = () => setTheme(window.__theme);
+  }, [theme]);
+
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
   let header
@@ -22,7 +35,39 @@ const Layout = ({ location, title, children }) => {
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
+      <header className="global-header">
+        {header}
+        {theme !== null ? (
+          <Toggle
+            icons={{
+              checked: (
+                <img
+                  src={moon}
+                  width="16"
+                  height="16"
+                  alt="Moon for dark mode"
+                  style={{ pointerEvents: 'none' }}
+                />
+              ),
+              unchecked: (
+                <img
+                  src={sun}
+                  width="16"
+                  height="16"
+                  alt="Sun for light mode"
+                  style={{ pointerEvents: 'none' }}
+                />
+              )
+            }}
+            checked={theme === 'dark'}
+            onChange={e => {
+              window.__setPreferredTheme(e.target.checked ? 'dark' : 'light');
+            }}
+          />
+        ) : (
+          <div style={{ height: '24px' }}/>
+        )}
+      </header>
       <main>{children}</main>
     </div>
   )
