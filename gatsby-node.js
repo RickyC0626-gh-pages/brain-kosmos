67eeeebd510a1a076f1df93292ff2886b3901f1d
Ricky Chon <rickychon99@gmail.com>
2022-01-11 12:49:34 -0500

    Remove onCreateNode completely to avoid node errs

--- gatsby-node.js
+++ gatsby-node.js
@@ -59,19 +59,6 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   }
 }
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-
-    createNodeField({
-      node,
-      value,
-    })
-  }
-}
-
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions
 
